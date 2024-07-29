b4126d0a67e303d8fde04e95413e356a287d53f8
CeniusM <93879193+CeniusM@users.noreply.github.com>
2024-07-30 00:09:26 +0200

    Added a bat script to run the url.py script with the python interpreter

---
+++ url.bat
@@ -0,0 +1,8 @@
+@REM Used to run the python script when typing "url" into the terminal
+
+@echo off
+set dir=%~dp0
+set file=url.py
+set file_path=%dir%%file%
+set args=%*
+python %file_path% %args%
